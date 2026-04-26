3b5595a557224c8aefd9f32f0be38fbf579b135b
fadelazka12-stack <fadelazka12@gmail.com>
2026-04-26 21:18:46 +0700

    push

--- api/proses/prosesLogin.php
+++ api/proses/prosesLogin.php
@@ -1,22 +1,29 @@
 <?php
 session_start();
 require __DIR__ .  '/../service/koneksi.php';
+
 $email = $_POST['email'];
 $password = $_POST['password'];
+
 $query = mysqli_query($koneksi, "SELECT * FROM users WHERE email='$email'");
-if(mysqli_num_rows($query)==1){
+
+if (mysqli_num_rows($query) == 1) {
     $user = mysqli_fetch_assoc($query);
-    if(password_verify($password, $user['password'])){
+    
+    if (password_verify($password, $user['password'])) {
         $_SESSION['id'] = $user['id'];
         $_SESSION['nama'] = $user['nama'];
         $_SESSION['role'] = $user['role'];
-        
-        // MATIKAN REDIRECT SEMENTARA UNTUK CEK
-        // header("Location: /api/dashboard.php");
-        echo "Login Berhasil!<br>";
-        var_dump($_SESSION);
-        die(); // Hentikan script sampai di sini
+        header("Location: /api/dashboard.php");
+        exit();
+    } else { 
+        $_SESSION['error'] = "Password salah"; 
+        header("Location: /api/login.php"); 
+        exit();
     }
-    } else { $_SESSION['error']="Password salah"; header("Location: /api/login.php"); }
- else { $_SESSION['error']="Email tidak terdaftar"; header("Location: /api/login.php"); }
+} else { 
+    $_SESSION['error'] = "Email tidak terdaftar"; 
+    header("Location: /api/login.php"); 
+    exit();
+}
 ?>
