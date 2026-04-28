5801445e09253afbe419ad3c23756e60a4cdd1fc
fadelazka12-stack <fadelazka12@gmail.com>
2026-04-28 12:03:41 +0700

    push

--- api/proses/ajax_handler.php
+++ api/proses/ajax_handler.php
@@ -218,7 +218,7 @@ if ($action === 'save') {
     // Kembalikan respons ke Javascript
     echo json_encode($response);
     exit();
-}
+}y
 
 // =====================================================================
 // 5. DELETE
